deda2c12824dd0e54f6199cfd49077f5f5b807aa
Henrique <h.gaiadematos@gmail.com>
2024-07-19 21:23:28 -0300

    Correções calculos

--- app/bot/calculo.py
+++ app/bot/calculo.py
@@ -4,6 +4,8 @@ from match import Match
 from data_house.odds_api import odds_api
 from data_house.sport_radar import sport_radar
 from house import House
+from datetime import datetime, timedelta
+from telegramApi import enviar_mensagem_telegram
 import math
 
 
@@ -20,9 +22,9 @@ def process_odd(odd_1, odd_2):
                 aposta_21_12 = (1 / match_odd_2.odds_a) + \
                     (1 / match_odd_1.odds_b)
                 surebet = aposta_11_22 < 1 or aposta_21_12 < 1
-
                 all_odds = [match_odd_1.odds_a,match_odd_1.odds_b,match_odd_2.odds_a,match_odd_2.odds_b]
                 check_surebet = {
+                    "data": match_odd_1.match_date,
                     "casa_a": match_odd_1.house,
                     "casa_b": match_odd_2.house,
                     "time_a": match_odd_1.time_a,
@@ -52,7 +54,6 @@ def process_odd(odd_1, odd_2):
 def run():
     # Aqui você pode adicionar mais fontes de dados, se necessário
     data_base = [odds_api,sport_radar]
-
     # Percorre os modelos
     for data in data_base:
         list_house_odds = []
@@ -76,12 +77,33 @@ def run():
         real_surebets = surebet_calculate.process_surebets(lista_possiveis_surebets)
         soma_totais = calculate_real_surebets(real_surebets)
         for real_surebet in real_surebets:
-            # Estou neste momento, faço os calculos da função abaixo calculate_real_surebets
-            # Após o calculo verificar se o calculo de locao esta correto e salvar os dados
-            # enviar para o telegram.
-            # locacao = (26.38/40.89)*(1-(0.17/1.66))*(1-(0.07/1.36))*100
-            locacao = (real_surebet['retorno_esperado']/soma_totais['total_retorno'])*(1-(soma_totais['total_c']/soma_totais['custo_transacao']))*(1-real_surebet['volatilidade']/soma_totais['total_volatilidade'])*100
-            print(locacao)
+            parte_1 = (real_surebet['retorno_esperado']/soma_totais['total_retorno']) 
+            parte_2 = (1-(soma_totais['custo_transacao']/soma_totais['total_c'])) 
+            parte_3 = (1-(real_surebet['volatilidade']/soma_totais['total_volatilidade']))
+            locacao = parte_1 * parte_2 * parte_3
+            real_surebet['locacao'] = locacao
+            locacao_time_1 = ((1/real_surebet['odds_a_time_b'])/real_surebet['surebet_total'])*locacao
+            real_surebet['locacao_proporcional_1'] = locacao_time_1
+            locacao_time_2 = ((1/real_surebet['odds_b_time_a'])/real_surebet['surebet_total'])*locacao
+            real_surebet['locacao_proporcional_2'] = locacao_time_2
+        if real_surebets:
+            for sb in real_surebets:
+                    message = (
+                        "**Oportunidade de Surebet encontrada:**\n"
+                        f"**Data:** {sb['data']}\n"
+                        f"**Partida:** {sb['time_a']} x {sb['time_b']}\n"
+                        f"**Tipo de Surebet:** {sb['surebet']}\n"
+                        f"**Retorno Esperado - R (%):** {sb['retorno_esperado']}\n"
+                        f"**Média das Odds:** {sb['media_odds']}\n"
+                        f"**Volatilidade - V:** {sb['volatilidade']}\n"
+                        f"**Alocação Surebets:** {sb['locacao']}\n"
+                        f"**Alocação Proporcional - Time 1:** {sb['locacao_proporcional_1']}\n"
+                        f"**Alocação Proporcional - Time 2:** {sb['locacao_proporcional_2']}\n"
+                        # f"**Lucro com investimento de R$ 10,00:** R$ {sb['lucro']:.2f}\n"
+                    )
+                    print(message)
+                    # Chame a função da sua API do Telegram para enviar a mensagem
+                    # enviar_mensagem_telegram(message)
 def calculate_real_surebets(surebet_calculate):
     custo_transacao = (49.9/30)/10
     total_retorno = 0
@@ -103,3 +125,12 @@ def calculate_real_surebets(surebet_calculate):
 # Exemplo de utilização:
 if __name__ == "__main__":
     run()
+
+
+
+# 1º a função calculate_real_surebet() deve somar apenas com as surbets do dia que vai ser enviados no grupo, dia atual e o proximo ou so dia atual
+# 2º Inclusão da comparação de empate
+# casa 1 TIME 1 casa 2 TIME 2 OU EMPATE
+# casa 1 TIME 2 casa 2 TIME 1 OU EMPATE
+# casa 1 TIME 1 OU EMPATE casa 2 TIME 2 
+# casa 1 TIME 2 u empate casa 2 time 1 
--- app/bot/calculoCustos.py
+++ app/bot/calculoCustos.py
@@ -1,40 +1,54 @@
+from datetime import datetime, timedelta
 class CalculoCustos:
     def __init__(self):
         pass
     def process_surebets(self, lista_possiveis_surebets):
         possible_surebets_list = lista_possiveis_surebets
         list_surebet = []
+        hoje = datetime.now().date()
+        amanha = hoje + timedelta(days=1)
         for surebet_key in possible_surebets_list:
-            if surebet_key['aposta_11_22'] < 1:
-                surebet = {
-                    'casa_a': surebet_key['casa_a'],
-                    'casa_b': surebet_key['casa_b'],
-                    'time_a': surebet_key['time_a'],
-                    'time_b': surebet_key['time_b'],
-                    'odds_a_time_a': surebet_key['odds_a_time_a'],
-                    'odds_b_time_a': surebet_key['odds_b_time_a'],
-                    'odds_a_time_b': surebet_key['odds_a_time_b'],
-                    'odds_b_time_b': surebet_key['odds_b_time_b'],
-                    'retorno_esperado': surebet_key['retorno_esperado_aposta_11_22'],
-                    'media_odds': surebet_key['media_odds'],
-                    'volatilidade': surebet_key['volatilidade'],
-                }
-                list_surebet.append(surebet)
-            if surebet_key['aposta_21_12'] < 1:
-                surebet = {
-                    'casa_a': surebet_key['casa_a'],
-                    'casa_b': surebet_key['casa_b'],
-                    'time_a': surebet_key['time_a'],
-                    'time_b': surebet_key['time_b'],
-                    'odds_a_time_a': surebet_key['odds_a_time_a'],
-                    'odds_b_time_a': surebet_key['odds_b_time_a'],
-                    'odds_a_time_b': surebet_key['odds_a_time_b'],
-                    'odds_b_time_b': surebet_key['odds_b_time_b'],
-                    'retorno_esperado': surebet_key['retorno_esperado_aposta_21_12'],
-                    'media_odds': surebet_key['media_odds'],
-                    'volatilidade': surebet_key['volatilidade'],
-                }
-                list_surebet.append(surebet)
+            try:
+                data_surebet = datetime.strptime(surebet_key['data'], '%d/%m/%y').date()
+            except:
+                data_surebet = datetime.strptime(surebet_key['data'], '%d/%m/%Y').date()
+            if data_surebet == hoje or data_surebet == amanha:
+                if surebet_key['aposta_11_22'] < 1:
+                    surebet = {
+                        'data' : surebet_key['data'],
+                        'casa_a': surebet_key['casa_a'],
+                        'casa_b': surebet_key['casa_b'],
+                        'time_a': surebet_key['time_a'],
+                        'time_b': surebet_key['time_b'],
+                        'odds_a_time_a': surebet_key['odds_a_time_a'],
+                        'odds_b_time_a': surebet_key['odds_b_time_a'],
+                        'odds_a_time_b': surebet_key['odds_a_time_b'],
+                        'odds_b_time_b': surebet_key['odds_b_time_b'],
+                        'surebet_total': surebet_key['aposta_11_22'],
+                        'retorno_esperado': surebet_key['retorno_esperado_aposta_11_22'],
+                        'media_odds': surebet_key['media_odds'],
+                        'volatilidade': surebet_key['volatilidade'],
+                        'surebet':surebet_key['casa_a']+ '-' + surebet_key['time_a']+'|'+surebet_key['casa_b']+'-'+surebet_key['time_b']
+                    }
+                    list_surebet.append(surebet)
+                if surebet_key['aposta_21_12'] < 1:
+                    surebet = {
+                        'data' : surebet_key['data'],
+                        'casa_a': surebet_key['casa_a'],
+                        'casa_b': surebet_key['casa_b'],
+                        'time_a': surebet_key['time_a'],
+                        'time_b': surebet_key['time_b'],
+                        'odds_a_time_a': surebet_key['odds_a_time_a'],
+                        'odds_b_time_a': surebet_key['odds_b_time_a'],
+                        'odds_a_time_b': surebet_key['odds_a_time_b'],
+                        'odds_b_time_b': surebet_key['odds_b_time_b'],
+                        'surebet_total': surebet_key['aposta_21_12'],
+                        'retorno_esperado': surebet_key['retorno_esperado_aposta_21_12'],
+                        'media_odds': surebet_key['media_odds'],
+                        'volatilidade': surebet_key['volatilidade'],
+                        'surebet':surebet_key['casa_a']+ '-' + surebet_key['time_b']+'|'+surebet_key['casa_b']+'-'+surebet_key['time_a']
+                    }
+                    list_surebet.append(surebet)
         return list_surebet
     
     
